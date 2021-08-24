6bff250794bfb18d42172f2303874c76ec3c2b35
Shenzz <Shenzz@qq.com>
2021-08-24 20:49:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shenzz
+ * @create 2021-08-24 20:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
